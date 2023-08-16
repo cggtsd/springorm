94dcbfb3193e8e892e3c13b7a5f614ae6d832ce1
CGG <cggtsds59@gmail.com>
2023-08-16 17:34:29 +0530

    commit 16-08-2023

--- src/main/java/cgg/spring/orm/App.java
+++ src/main/java/cgg/spring/orm/App.java
@@ -1,5 +1,10 @@
 package cgg.spring.orm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,13 +22,116 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         StudentDao studentDao = context.getBean("studentdao",StudentDao.class);
         
-        Student student = new Student();
-        student.setStudentId(2345);
-        student.setStudentName("Fathima");
-        student.setStudentCity("hyd");
+//        Student student = new Student();
+//        student.setStudentId(2345);
+//        student.setStudentName("Fathima");
+//        student.setStudentCity("hyd");
+//        
+//        int r = studentDao.insert(student);
+//        System.out.println("done "+r);
         
-        int r = studentDao.insert(student);
-        System.out.println("done "+r);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean go = true;
+        while(go) {
+        	System.out.println("PRESS 1 for add new Student");
+        	System.out.println("PRESS 2 for displau all Students");
+        	System.out.println("PRESS 3  get detail of Single Student");
+        	System.out.println("PRESS 4 for delete Student");
+        	System.out.println("PRESS 5 for update Student");
+        	System.out.println("PRESS 6 for exit");
+        	
+        	try {
+				int input=Integer.parseInt(br.readLine());
+				switch(input) {
+				case 1: //add a new student
+					    System.out.println("Enter student id: ");
+					    int uId = Integer.parseInt(br.readLine());
+				
+					    System.out.println("Enter student name");
+					    String name = br.readLine();
+					    
+					    System.out.println("Enter student city");
+					    String city = br.readLine();
+					    
+					    Student student = new Student();
+					    student.setStudentId(uId);
+					    student.setStudentName(name);
+					    student.setStudentCity(city);
+					    
+					    int r = studentDao.insert(student);
+					    System.out.println(r+" student added");
+					    
+					    System.out.println("******************************");
+					    System.out.println();
+					    break;
+					    
+					    case 2: 
+					    	    //display all students
+					    	List<Student> allStudents = studentDao.getAllStudents();
+					         for (Student st : allStudents) {
+								System.out.println("Id :       "+st.getStudentId());
+								System.out.println("Name :     "+st.getStudentName());
+								System.out.println("City :     "+st.getStudentCity());
+								System.out.println("_________________________________");
+							}
+					         
+					         break;
+					         
+					    case 3: //get single student data
+					    	System.out.println("Enter student id :");
+						    int studId = Integer.parseInt(br.readLine());
+						    
+						    Student stud  = studentDao.getStudent(studId);
+						    System.out.println("Id :       "+stud.getStudentId());
+							System.out.println("Name :     "+stud.getStudentName());
+							System.out.println("City :     "+stud.getStudentCity());
+							System.out.println("_________________________________");
+							break;
+					    
+					    case 4: // delete student
+					    	System.out.println("Enter student id :");
+						    int id = Integer.parseInt(br.readLine());
+						    
+						    studentDao.delete(id);
+						    System.out.println("Student deleted.....");
+						    break;
+						    
+					    case 5: //update student
+					    	System.out.println("Enter student id :");
+						    id = Integer.parseInt(br.readLine());
+						    
+						    System.out.println("Enter student name");
+						    name = br.readLine();
+						    
+						    System.out.println("Enter student city");
+						    city = br.readLine();
+						    
+						    student = new Student();
+						    student.setStudentId(id);
+						    student.setStudentName(name);
+						    student.setStudentCity(city);
+						    
+						    studentDao.update(student);
+						    System.out.println("Student Updated .....");
+						    break;
+						    
+						    case 6: //exit
+						    	   go= false;
+						    	   break;
+						    
+				
+				}
+				
+			} catch (NumberFormatException | IOException e) {
+				System.out.println("Invalid input try with another one !!");
+				System.out.println(e.getMessage());
+			}
+        	
+        }
+        
+      System.out.println("Thankyou for using my application");
+      System.out.println("see you soon");
+        
         
     }
 }
--- src/main/java/cgg/spring/orm/dao/StudentDaoImpl.java
+++ src/main/java/cgg/spring/orm/dao/StudentDaoImpl.java
@@ -1,5 +1,7 @@
 package cgg.spring.orm.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +31,36 @@ public class StudentDaoImpl implements StudentDao {
 		//insert
 		Integer i =(Integer)hibernateTemplate.save(student);
 		return i;
+	}
+
+	//updating data...
+	@Override
+	@Transactional
+	public void update(Student student) {
+		this.hibernateTemplate.update(student);
+    }
+
+	//deleting the data...
+	@Override
+	@Transactional
+	public void delete(int studentId) {
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		this.hibernateTemplate.delete(student);
+		
+	}
+
+	//get the single data(object)
+	@Override
+	public Student getStudent(int studentId) {
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		return student;
+	}
+
+	//get all students (all rows)
+	@Override
+	public List<Student> getAllStudents() {
+		List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+		return students;
 	}
 
 }
